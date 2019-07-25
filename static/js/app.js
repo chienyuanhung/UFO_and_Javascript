331724255f6efa764a9d417e926129d5a02d3cdd
chienyuanhung <chien.yuanhung@gmail.com>
2019-07-25 00:07:39 -0700

    do multiple choice

--- static/js/app.js
+++ static/js/app.js
@@ -14,23 +14,45 @@ tableData.forEach((data) => {
   });
 
 
- // Getting a reference to the button on the page with the class
-var dateButton = d3.select(".btn_datetime");
-var cityButton = d3.select(".btn_city");
-var stateButton = d3.select(".btn_state");
-var countryButton = d3.select(".btn_country");
-var shapeButton = d3.select(".btn_shape");
-
+ // Getting a reference to the button on the page with the id filter-btn
+var button = d3.select("#filter-btn");
   
-// datetime button
-dateButton.on("click", function() {
 
-     // Getting a reference to the date input element on the page with the id set to 'datetime'
-     var dateInput = d3.select("#datetime");
+//button
+button.on("click", function() {
 
-     var dataInputValue = dateInput.property("value");
 
-     var filterData = tableData.filter(myData => myData.datetime === dataInputValue);
+     // Getting a reference to the date input value on the page with the id 
+     var dateInput = d3.select("#datetime").property("value");
+     var cityInput = d3.select("#city").property("value");
+     var stateInput = d3.select("#state").property("value");
+     var countryInput = d3.select("#country").property("value");
+     var shapeInput = d3.select("#shape").property("value");
+
+     // get the filter data
+     var filterData = tableData
+
+     // filter data by input conditions, if exist
+     if (dateInput){
+      filterData = filterData.filter(myData => myData.datetime === dateInput);
+     };
+     
+     if (cityInput){
+      filterData = filterData.filter(myData => myData.city === cityInput);
+     };
+
+     if (stateInput){
+      filterData = filterData.filter(myData => myData.state === stateInput);
+     };
+
+     if (countryInput){
+      filterData = filterData.filter(myData => myData.country === countryInput);
+     };
+
+     if (shapeInput){
+      filterData = filterData.filter(myData => myData.shape === shapeInput);
+     };
+
 
      tbody.html(" ")
 
@@ -43,97 +65,98 @@ dateButton.on("click", function() {
          });
       });
   
+
   });
     
 
-// city button
-cityButton.on("click", function() {
+// // city button
+// cityButton.on("click", function() {
 
-     // Getting a reference to the date input element on the page with the id set to 'datetime'
-     var cityInput = d3.select("#city");
+//      // Getting a reference to the date input element on the page with the id set to 'datetime'
+//      var cityInput = d3.select("#city");
 
-     var dataInputValue = cityInput.property("value");
+//      var dataInputValue = cityInput.property("value");
 
-     var filterData = tableData.filter(myData => myData.city === dataInputValue);
+//      var filterData = tableData.filter(myData => myData.city === dataInputValue);
 
-     tbody.html(" ")
+//      tbody.html(" ")
 
-     //construct the table
-     filterData.forEach((data) => {
-         var row = tbody.append("tr");
-          Object.entries(data).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-         });
-      });
+//      //construct the table
+//      filterData.forEach((data) => {
+//          var row = tbody.append("tr");
+//           Object.entries(data).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//          });
+//       });
   
-  });
+//   });
     
-// state button
-stateButton.on("click", function() {
+// // state button
+// Button.on("click", function() {
 
-  // Getting a reference to the date input element on the page with the id set to 'datetime'
-  var stateInput = d3.select("#state");
+//   // Getting a reference to the date input element on the page with the id set to 'datetime'
+//   var stateInput = d3.select("#state");
 
-  var dataInputValue = stateInput.property("value");
+//   var dateInputValue = stateInput.property("value");
 
-  var filterData = tableData.filter(myData => myData.state === dataInputValue);
+//   var filterDate = tableData.filter(myData => myData.state === dateInputValue);
 
-  tbody.html(" ")
+//   tbody.html(" ")
 
-  //construct the table
-  filterData.forEach((data) => {
-      var row = tbody.append("tr");
-       Object.entries(data).forEach(([key, value]) => {
-         var cell = row.append("td");
-         cell.text(value);
-      });
-   });
+//   //construct the table
+//   filterData.forEach((data) => {
+//       var row = tbody.append("tr");
+//        Object.entries(data).forEach(([key, value]) => {
+//          var cell = row.append("td");
+//          cell.text(value);
+//       });
+//    });
 
-});
+// });
 
-// country button
-countryButton.on("click", function() {
+// // country button
+// countryButton.on("click", function() {
 
-  // Getting a reference to the date input element on the page with the id set to 'datetime'
-  var countryInput = d3.select("#country");
+//   // Getting a reference to the date input element on the page with the id set to 'datetime'
+//   var countryInput = d3.select("#country");
 
-  var dataInputValue = countryInput.property("value");
+//   var dataInputValue = countryInput.property("value");
 
-  var filterData = tableData.filter(myData => myData.country === dataInputValue);
+//   var filterData = tableData.filter(myData => myData.country === dataInputValue);
 
-  tbody.html(" ")
+//   tbody.html(" ")
 
-  //construct the table
-  filterData.forEach((data) => {
-      var row = tbody.append("tr");
-       Object.entries(data).forEach(([key, value]) => {
-         var cell = row.append("td");
-         cell.text(value);
-      });
-   });
+//   //construct the table
+//   filterData.forEach((data) => {
+//       var row = tbody.append("tr");
+//        Object.entries(data).forEach(([key, value]) => {
+//          var cell = row.append("td");
+//          cell.text(value);
+//       });
+//    });
 
-});
+// });
 
-// shape button
-shapeButton.on("click", function() {
+// // shape button
+// shapeButton.on("click", function() {
 
-  // Getting a reference to the date input element on the page with the id set to 'datetime'
-  var shapeInput = d3.select("#shape");
+//   // Getting a reference to the date input element on the page with the id set to 'datetime'
+//   var shapeInput = d3.select("#shape");
 
-  var dataInputValue = shapeInput.property("value");
+//   var dataInputValue = shapeInput.property("value");
 
-  var filterData = tableData.filter(myData => myData.shape === dataInputValue);
+//   var filterData = tableData.filter(myData => myData.shape === dataInputValue);
 
-  tbody.html(" ")
+//   tbody.html(" ")
 
-  //construct the table
-  filterData.forEach((data) => {
-      var row = tbody.append("tr");
-       Object.entries(data).forEach(([key, value]) => {
-         var cell = row.append("td");
-         cell.text(value);
-      });
-   });
+//   //construct the table
+//   filterData.forEach((data) => {
+//       var row = tbody.append("tr");
+//        Object.entries(data).forEach(([key, value]) => {
+//          var cell = row.append("td");
+//          cell.text(value);
+//       });
+//    });
 
-});
+// });
